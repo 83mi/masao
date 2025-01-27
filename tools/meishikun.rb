47459d398c47156cfe28c171833380c378e014d4
83mi <yasumi@yokoya.org>
2025-01-27 19:36:02 +0900

    テスト元データ形式の変更

--- tools/meishikun.rb
+++ tools/meishikun.rb
@@ -2,10 +2,10 @@
 
 require 'json'
 
-data = JSON.parse(open("Data.json").read)
+data = JSON.parse(open("item_name.json").read)
 template = "NAME,1288,1288,4000,名詞,固有名詞,一般,*,*,*,アイテム,アイテム,アイテム"
 
 data.each do |d|
-  name = d.last["Name"]
+  name = d
   puts template.sub(/^NAME/, name)
 end
